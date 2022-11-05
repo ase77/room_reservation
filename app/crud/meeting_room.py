c85139b2bf8683e8d335b8d6ab41bf80044240c3
Моторин Антон <ust.mir@gmail.com>
2022-11-05 19:06:08 +0300

    add mitingroom

---
+++ app/crud/meeting_room.py
@@ -0,0 +1,117 @@
+from typing import Optional
+
+# Добавляем импорт функции select.
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.encoders import jsonable_encoder
+
+# Импортируем sessionmaker из файла с настройками БД.
+from app.core.db import AsyncSessionLocal
+from app.models.meeting_room import MeetingRoom
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
+
+
+# Функция работает с асинхронной сессией,
+# поэтому ставим ключевое слово async.
+# В функцию передаём схему MeetingRoomCreate.
+async def create_meeting_room(
+        new_room: MeetingRoomCreate,
+        session: AsyncSession,
+) -> MeetingRoom:
+    # Конвертируем объект MeetingRoomCreate в словарь.
+    new_room_data = new_room.dict()
+
+    # Создаём объект модели MeetingRoom.
+    # В параметры передаём пары "ключ=значение", для этого распаковываем словарь.
+    db_room = MeetingRoom(**new_room_data)
+
+    # Добавляем созданный объект в сессию.
+    # Никакие действия с базой пока ещё не выполняются.
+    session.add(db_room)
+    # Записываем изменения непосредственно в БД.
+    # Так как сессия асинхронная, используем ключевое слово await.
+    await session.commit()
+    # Обновляем объект db_room: считываем данные из БД, чтобы получить его id.
+    await session.refresh(db_room)
+    # Возвращаем только что созданный объект класса MeetingRoom.
+    return db_room
+
+
+async def get_room_id_by_name(
+    room_name: str,
+    session: AsyncSession,
+) -> Optional[int]:
+    # Получаем объект класса Result.
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(
+            MeetingRoom.name == room_name
+        )
+    )
+    # Извлекаем из него конкретное значение.
+    db_room_id = db_room_id.scalars().first()
+    return db_room_id
+
+
+async def read_all_rooms_from_db(session: AsyncSession) -> list[MeetingRoom]:
+    db_rooms = await session.execute(select(MeetingRoom))
+    db_rooms = db_rooms.scalars().all()
+    return db_rooms
+
+
+async def get_meeting_room_by_id(
+    room_id: int,
+    session: AsyncSession,
+) -> Optional[MeetingRoom]:
+    # Получаем объект класса Result.
+    db_room = await session.execute(
+        select(MeetingRoom).where(
+            MeetingRoom.id == room_id
+        )
+    )
+    # Извлекаем из него конкретное значение.
+    db_room = db_room.scalars().first()
+    return db_room
+    # Есть более короткий способ получить объект по его id
+    # db_room = await session.get(MeetingRoom, room_id)
+    # return db_room
+
+
+async def update_meeting_room(
+        # Объект из БД для обновления.
+        db_room: MeetingRoom,
+        # Объект из запроса.
+        room_in: MeetingRoomUpdate,
+        session: AsyncSession,
+) -> MeetingRoom:
+    # Представляем объект из БД в виде словаря.
+    obj_data = jsonable_encoder(db_room)
+    # Конвертируем объект с данными из запроса в словарь,
+    # исключаем неустановленные пользователем поля.
+    update_data = room_in.dict(exclude_unset=True)
+
+    # Перебираем все ключи словаря, сформированного из БД-объекта.
+    for field in obj_data:
+        # Если конкретное поле есть в словаре с данными из запроса, то...
+        if field in update_data:
+            # ...устанавливаем объекту БД новое значение атрибута.
+            setattr(db_room, field, update_data[field])
+    # Добавляем обновленный объект в сессию.
+    session.add(db_room)
+    # Фиксируем изменения.
+    await session.commit()
+    # Обновляем объект из БД.
+    await session.refresh(db_room)
+    return db_room
+
+
+async def delete_meeting_room(
+        db_room: MeetingRoom,
+        session: AsyncSession,
+) -> MeetingRoom:
+    # Удаляем объект из БД.
+    await session.delete(db_room)
+    # Фиксируем изменения в БД.
+    await session.commit()
+    # Не обновляем объект через метод refresh(),
+    # следовательно он всё ещё содержит информацию об удаляемом объекте.
+    return db_room
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.api.meeting_room import router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
+
+app.include_router(router)
